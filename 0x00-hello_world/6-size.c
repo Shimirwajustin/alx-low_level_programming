787e9aa19d0c55cec7d120accbf4338b344c8c81
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-16 04:21:39 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,7 @@ long long int lld;
 float f;
 printf("size of char:%lu byte(s)\n",(unsigned long)sizeof(c));
 printf("size of int: %lu byte(s)\n",(unsigned long)sizeof(d));
-print("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(ld));
-print("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lld));
+printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(ld));
+printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lld));
 printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
-}
