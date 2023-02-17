c9cc7e108861932ea28a005677877565051e2267
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 12:27:39 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,11 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/**
-*main - determines whether randomly generated number is odd or even
-*
-*Return: 0 if program executes succesfuly
-*/
+
 int main(void)
 {
 	int n;
@@ -21,8 +17,13 @@ int main(void)
         printf("Last digit of %d is %d and is greater than 5\n", n, r);
     else if (r == 0)
             printf("Last digit of %d is %d and is 0\n", n, r);
-    else if (r < 6 && r != 0)
-            printf("Last digit of %d is %d and is less than 6 and not 0", n, r);
-     
-    return (0);
+    else if (r < 6)
+    {
+      if (r != 0)
+         printf("Last digit of %d is %d and is less than 6 and not 0", n, r);
+         else
+         printf(" nothing")
+    }
+    
+return (0);
 }
