e6767ded01ca018e59d4c36ef8847d17c59d09ac
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 06:12:41 -0800

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
