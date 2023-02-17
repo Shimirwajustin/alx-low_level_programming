b1f168c0428cd3e08a18ea882368ab4cf0874796
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 14:43:06 -0800

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Prints the alphabet
+*
+* Return: 0 when code executes succesfully
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{for (c != 'q' && c != 'e')
+putchar (c);
+}
+putchar ('\n');
+return (0);
+}
