62686d379113d4a3c4ed9b972bce796ddf27d2a9
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 13:49:13 -0800

    The alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-int main()
+int main(void)
 {
 char c;
-for (c = 'A'; c <= 'Z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
-putchar(c);
+putchar (c);
 }
 return (0);
 }
