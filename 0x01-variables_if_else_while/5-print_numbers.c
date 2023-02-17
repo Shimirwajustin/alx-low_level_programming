04902423f5414152bc8080a34778c8de488b6e1e
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 15:25:54 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - determines whether randomly generated number is odd or even
+*
+*Return: 0 if program executes succesfuly
+*/
+int main()
+{
+int a;
+for (a = 0; a <= 10; a++)
+printf("%d\n", a);
+return (0);
+}
