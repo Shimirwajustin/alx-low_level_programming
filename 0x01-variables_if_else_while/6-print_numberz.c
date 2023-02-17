3a8b15e0f530da2f7bf1f673c290562caac1661e
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 15:47:42 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+*main - determines whether randomly generated number is odd or even
+*
+*Return: 0 if program executes succesfuly
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
