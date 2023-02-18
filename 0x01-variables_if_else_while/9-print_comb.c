0ab078d5dd3537bad5433ce72dbaaac8c395925e
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-17 16:49:43 -0800

    An unbeatable combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Prints the alphabet
+*
+* Return: 0 when code executes succesfully
+*/
+int main(void)
+{
+char c;
+for (c = 0; c < 10; c++)
+putchar (c + '0');
+putchar (',');
+putchar ('');
+putchar ('\n');
+return (0);
+}
