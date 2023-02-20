75d4ccb16f653ea9d59b6ade5329b966e81476aa
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-20 01:34:39 -0800

    possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+/*file name = 102-print_comb5.c
+*
+*Auth: Shimirwa Justin
+*/
+/**
+*main - Prints all possible combinations of two two-digit numbers,
+* ranging from 0-99, separated by a comma followed by a space.
+*
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c, h;
+for (c = 0; c <= 98; c++)
+{
+for (h = c + 1; h <= 99; h++)
+{
+putchar ((c / 10) + '0');
+putchar ((c % 10) + '0');
+putchar (' ');
+putchar ((h / 10) + '0');
+putchar ((c % 10) + '0');
+if (c == 98 &&& h == 99)
+continue;
+putchar (',');
+putchar (',');
+}
+}
+putchar ('\n');
+return (0);
+}
