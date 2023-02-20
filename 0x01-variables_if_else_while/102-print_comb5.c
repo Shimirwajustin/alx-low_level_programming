805381ced0891b86f1f7c3be93c78dd780741f67
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-20 01:44:24 -0800

    possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,17 +15,17 @@ for (c = 0; c <= 98; c++)
 {
 for (h = c + 1; h <= 99; h++)
 {
-putchar ((c / 10) + '0');
-putchar ((c % 10) + '0');
-putchar (' ');
-putchar ((h / 10) + '0');
-putchar ((c % 10) + '0');
+putchar((c / 10) + '0');
+putchar((c % 10) + '0');
+putchar(' ');
+putchar((h / 10) + '0');
+putchar((c % 10) + '0');
 if (c == 98 &&& h == 99)
 continue;
-putchar (',');
-putchar (',');
+putchar(',');
+putchar(' ');
 }
 }
-putchar ('\n');
+putchar('\n');
 return (0);
 }
