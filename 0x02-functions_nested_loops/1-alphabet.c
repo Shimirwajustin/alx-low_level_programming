cea973451e995baf277c582ed153cfe128c0a418
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-21 08:15:32 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char j = 'a';
+while (j <= 'z')
+{
+_putchar(j);
+j++;
+}
+_putchar('\n');
+}
