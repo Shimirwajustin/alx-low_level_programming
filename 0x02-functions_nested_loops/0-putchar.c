8da50a723d4837011a07bfa219c118b60a7e2506
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-21 13:30:53 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
-*_putchar: print putchar alphabetic putchar
+*_putchar: print putchar alphabetic putchar 
 *return : always 0
 *
 */
 int main(void)
 {
 _putchar('_');
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
 _putchar('\n');
 return (0);
 }
