319e6480ffcc13c8582c5931c149e18d6cd5bbc5
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-21 16:29:54 -0800

    A function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
+/* betty style doc for function main goes there */
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+*_isalpha - alphabet function
+*@n: character to be printed
+*
+*putchar:print the sign
+*Return: Always 0
+*/
+int print_sign(int n)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+ if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+_putchar(',');
+_putchar(' ');
+return (-1);
+}
 }
