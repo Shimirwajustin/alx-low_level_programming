0399ddb865338d787905e01589892e004552a785
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-21 18:35:09 -0800

    a function that prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ if (n >= 0 && n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
-printf("%d", i);
+printf("%d, ", i);
 }
 printf("\n");
 }
@@ -23,18 +23,18 @@ else if (n < 0)
 {
 for (i = n; i <= 98; i++)
 {
-printf("%d", i);
+printf("%d, ", i);
 }
 printf("\n");
 }
 else if (n > 98)
 {
-for (i = n; i >= 98; i++)
+for (i = n; i >= 98; i--)
 {
-printf("%d", i);
+printf("%d, ", i);
 }
 printf("\n");
 }
 else if (n == 98)
-printf("%d", n);
+printf("%d, ", n);
 }
