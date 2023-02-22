afbcd73542a670d446e9ab12f6cf9db741f27022
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-21 16:38:29 -0800

    A function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,11 @@
 
 /* betty style doc for function main goes there */
 /**
-*_isalpha - alphabet function
+*print_sign - function that prints signs
 *
-*putchar:print the sign
-*Return: Always 0
+*@n: function parameter
+*
+*Return: 1, 0 and -1
 */
 int print_sign(int n)
 {
@@ -23,7 +24,7 @@ _putchar(',');
 _putchar(' ');
 return (0);
 }
-else if (n < 0)
+else (n < 0)
 {
 _putchar('-');
 _putchar(',');
