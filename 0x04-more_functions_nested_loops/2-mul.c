0853dd5109e6f85b4b1b8db3fcb52d60b09d17f9
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 02:36:51 -0800

    function to print multiplication.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,14 +6,9 @@
 *mul - function to print multiplication
 *@a: the character needed to multiply
 *@b: the character needed to multiply
-*Return: always 0
+*Return: multiplication
 */
 int mul(int a, int b)
 {
-int answer;
-a = 5;
-b = 9;
-answer = a *b;
-_putchar(answer);
-return (0);
+return (a * b);
 }
