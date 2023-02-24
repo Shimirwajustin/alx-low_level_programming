6719a1ce4c52389e9c1df307f6a47fe80e827d60
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 03:28:43 -0800

    function to print the numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,15 +4,14 @@
 /*betty style*/
 /**
 *print_numbers - function to print the numbers from 0 to 9
-*Return: always 0
+*Return: void
 */
 void print_numbers(void)
 {
 int b;
 for (b >= 0; b <= 9; b++)
 {
-_putchar(b);
+_putchar(b + '0');
 }
 _putchar('\n');
-return (0);
 }
