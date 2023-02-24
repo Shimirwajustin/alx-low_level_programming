f25ac90bb22e319f8e0bd5f5cfd0ea17af5ada6c
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 02:54:46 -0800

    function to print the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/*betty style*/
+/**
+*print_numbers - function to print the numbers from 0 to 9
+*Return: no return
+*/
+void print_numbers(void)
+{
+int b;
+for (b >= '0'; b <= '9'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
