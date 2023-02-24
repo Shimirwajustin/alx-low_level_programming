232c3f05cc79ff133d96f0447ac3424860fd5736
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 02:33:38 -0800

    function to print multiplication.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/*betty style*/
+/**
+*mul - function to print multiplication
+*@a: the character needed to multiply
+*@b: the character needed to multiply
+*Return: always 0
+*/
+int mul(int a, int b)
+{
+int answer;
+a = 5;
+b = 9;
+answer = a *b;
+_putchar(answer);
+return (0);
+}
