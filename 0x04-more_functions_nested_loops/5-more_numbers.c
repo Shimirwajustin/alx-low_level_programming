5949045998a50e4a1e703590d4dd0db84c3732d5
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 04:28:17 -0800

    a function that prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ int a;
 int b;
 for (a = 0; a < 10; a++)
 {
-for (b =0; b <= 14; b++)
+for (b =0; b < 15; b++)
 {
 if (b > 9)
 _putchar((b / 10) + '0');
-_putchar((b % 10) + '0')
+_putchar((b % 10) + '0');
 }
 _putchar('\n');
 }
