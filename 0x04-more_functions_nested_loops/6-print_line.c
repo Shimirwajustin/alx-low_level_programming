bde6df1cab040518c7a826d64772e3e22374e1c4
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-24 06:08:32 -0800

    a function that draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/*betty style*/
+/**
+*print_line -  a function that draws a straight line
+*@n: to be checked
+*Return: void
+*/
+void print_line(int n)
+{
+int a = 0;
+
+if (a <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+_putchar('_');
+}
+_putchar('\n');
+}
