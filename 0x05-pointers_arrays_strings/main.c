b4ece94d8669984948c3ff0b94f8c1358b00f2eb
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-27 13:47:51 -0800

    main h

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,13 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-#endif
+#include <stdio.h>
+
+void _puts(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Holberton!";
+	_puts(str);
+	return (0);
