7fd32d20c8b30f68bb694349774265fd8ce85467
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-27 06:52:43 -0800

    a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+/**
+* _strlen - a function that returns the length of a string
+*@str: a variable used to print string
+* Return: str.
+*/
+void _puts(char *str)
+{
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (str);
+}
