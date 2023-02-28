919028d2e7ce7e0188ab59ccc115d7fd3a1fbed1
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-28 14:33:18 -0800

    program that generates random valid passwords

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,28 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
-* main - program that generates random valid passwords
-* Return: Always 0
+* print_array - a function that prints n elements of an array
+* @a: array name
+* @n: is the number of elements OF the array to be printed
+* Return: a and n inputs
 */
-int main(void)
+void print_array(int *a, int n)
 {
-int pass[100];
-int a, sum, b;
-sum = 0;        
-srand(time(NULL));
-for (a = 0; a < 100; a++)
+int b;
+for (b = 0; b < (n - 1); b++)
 {
-pass[a] = rand() % 78;
-sum += (pass[a] + '0');
-putchar(pass[a] + '0');
-if ((2772 - sum) - '0' < 78)
-{
-b = 2772 - sum - '0';
-sum += b;
-putchar(b + '0');
-break;
+printf("%d, ", a[b]);
 }
+if (b == (n - 1))
+{
+printf("%d", a[n - 1]);
 }
-return (0);
+printf("\n");
 }
