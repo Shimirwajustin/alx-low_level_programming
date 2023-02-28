26efcf6ab827b66af773d826d1bf1da8cca1c9d5
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-28 02:37:43 -0800

    a function should print only one character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-*puts2 - a function that prints every other character of a string
+*puts2 - function should print only one character out of two
+*starting with the first one
 *@str: string
 * Return: void.
 */
@@ -9,12 +10,20 @@ void puts2(char *str)
 {
 int leng = 0;
 int a = 0;
-while (str[leng] != '\0')
+char *y = str;
+int b;
+while (*y != '\0')
+{
+y++;
 leng++;
-while (a < leng)
+}
+a = leng - 1;
+for (b = 0; b <= a; b++)
+{
+if (b % 2 == 0)
 {
 _putchar(str[0]);
-a = a + 2;
+}
 }
 _putchar('\n');
 }
