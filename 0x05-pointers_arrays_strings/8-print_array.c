ad3bb88890ec96baddd1cd5bc6d1c5a6b650fef5
Shimirwa Justin <shimirwajustin@gmail.com>
2023-02-28 14:22:36 -0800

    program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - program that generates random valid passwords
+* Return: Always 0
+*/
+int main(void)
+{
+int pass[100];
+int a, sum, b;
+sum = 0;        
+srand(time(NULL));
+for a = 0; a < 100; a++)
+{
+pass[a] = rand() % 78;
+sum += (pass[a] + '0');
+putchar(pass[a] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += b;
+putchar(b + '0');
+break;
+}
+}
+return (0);
+}
