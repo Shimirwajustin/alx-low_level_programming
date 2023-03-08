10b8ba34be9bb8e2a4c63cced8db3ae10f7e133c
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-08 09:12:55 -0800

    a function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_puts_recursion - a function to reverse a string
+*@s: an input
+*Return: not always 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s + 1);
+putchar(*s);
+}
+}
