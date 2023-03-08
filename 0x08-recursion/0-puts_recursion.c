4ab4c479168670d01382dfcd7dea2287154bfed1
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-08 10:12:45 -0800

    Read me file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-_puts_recursion(s + 1);
-_puts_recursion(*s);
+_putchar('\n');
+return;
 }
-putchar('\n');
+_putchar(*s);
+_puts_recursion(s + 1);
 }
