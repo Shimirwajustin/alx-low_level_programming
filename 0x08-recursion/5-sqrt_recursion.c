fbe4b305892f5b31a96782436c24eea3e02924f6
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-09 09:21:49 -0800

    a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,11 @@
 *_sqrt_recursion - a function
 *@n: input
 *Return: not always 0
+*actual_sqrt_recursion - a function that
+*recurses to find the square root of
+*natural number
+*@a: input
+*Return: square root
 */
 int actual_sqrt_recursion(int n, int a);
 int _sqrt_recursion(int n)
@@ -13,19 +18,11 @@ if (n < 0)
 return (-1);
 return (actual_sqrt_recursion(n, 0));
 }
-/**
-*actual_sqrt_recursion - a function that
-*recurses to find the square root of
-*natural number
-*@n: input
-* @a: input
-*Return: square rot
-*/
 int actual_sqrt_recursion(int n, int a)
 {
 if (a * a > n)
 return (-1);
 if (a * a == n)
 return (a);
-return(actual_sqrt_recursion(n, a + 1));
+return (actual_sqrt_recursion(n, a + 1));
 }
