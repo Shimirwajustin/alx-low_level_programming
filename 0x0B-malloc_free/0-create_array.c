8355849165b8482f978ec862657d055c271d98db
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-13 12:36:08 -0700

    a function that creates an array of character.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+*create_array - function
+*@size: input
+*@c: input
+*Retirn: not always 0
+*/
+char *create_array(unsigned int size, char c)
+{
+if (size = 0)
+return (NULL);
+else
+return create_array;
+}
