7d99cf6df9d8f7ae627d1329a0e9e3ed91aafb30
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-15 00:17:37 -0700

    string concatenation

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+* _strncat - a function that concatenates two strings.
+*@dest: string
+*@src: string
+*@n: integer
+* Return: dest.
+*/
+#include "main.h"
+char *_strncat(char *dest, char *src, int n)
+{
+int a;
+int b;
+a = 0;
+b = 0;
+while ( dest[a] != '\0')
+{
+a++;
+}
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
+
