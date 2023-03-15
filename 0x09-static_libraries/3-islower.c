4988a4b5a86a01adce6d164fa4f6383eea3ce891
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-15 04:37:18 -0700

    a function tha checkes lower character

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+*_islower - alphabet function
+*@c: an input
+*Return: 1 and 0
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
