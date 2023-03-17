8666f6bba8517544b0c1e5ccb70bbbb708caf8b6
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-17 07:27:36 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,44 @@
 #include <stdio.h>
 #include <string.h>
 /**
+*atoi - a function
+*@s: character
+*Return: 0 and n
+*/
+int atoi(char *s)
+{
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if(s[i] == '-')
+++d;
+if(s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if(s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+
+/**
 *main - a function
 *@argc: argument
 *@argv: argument of an array
@@ -16,8 +54,8 @@ if(argc < 3 || argc > 3)
 printf("Error\n");
 return(1);
 }
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 result = a*b;
 printf("%d", result);
 return (0);
