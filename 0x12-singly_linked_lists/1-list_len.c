1a02721af16c44ec227b58fa4d45e514f57620d6
Shimirwa Justin <shimirwajustin@gmail.com>
2023-03-31 05:01:27 -0700

    file of 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *list_len - return the number of
+ *elements in a linked list
+ *@h: pointer
+ *Return: not always 0
+ */
+size_t list_len(const list_t *h)
+{
+size_t n = 0;
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
+}
