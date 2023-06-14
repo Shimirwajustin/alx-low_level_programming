2f27fc1dd18bd90aa51957b60be88c36a64b4776
Shimirwa Justin <shimirwajustin@gmail.com>
2023-06-14 10:00:44 +0200

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,23 +4,23 @@
 /**
 *main - function
 *@argc: integer
-*@ argv: array
+*@argv: array
 *Return: 1 or 0
 */
 int main(int argc, char *argv[])
 {
-{
+int a;
+char b[];
+a = 0;
+b[]= "Error";
 if (argc != 3)
-printf("error");
+{
+printf("%s", b);
 printf("\n");
 return 1;
 }
-int a, b;
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-int c;
-c = a * b;
-printf("%d", result);
+a = atoi(argv[1]) * atoi(argv[2]);
+printf("%i", a);
 printf("\n");
 return 0;
 }
