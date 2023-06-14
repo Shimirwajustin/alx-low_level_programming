ebc470af9ba7f19314c687ecd4caaf94378ddd9b
Shimirwa Justin <shimirwajustin@gmail.com>
2023-06-14 12:12:06 +0200

    argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- function
+*@argc: integer
+*@argv: an array
+*Return: 1 & 0
+*/
+int main(int argc, char *argv[])
+{
+int a;
+int b = 0;
+int c = 0;
+char d[] = "Error";
+char *e;
+if (argc > 1)
+{
+for (a = 1; a < argc; a++)
+{
+e = argv[1];
+while (*e != 0)
+{
+if (*e < 47 || *e > 57)
+{
+printf("%s\n", d);
+return 1;
+}
+e++;
+}
+c = atoi(argv[a]);
+b += c;
+}
+printf("%d\n", b);
+}
+else
+printf("%d\n", 0);
+return (0);
+}
+
