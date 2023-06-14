dd0b260cdd6b343819765fc6cc099b1f491f6e39
Shimirwa Justin <shimirwajustin@gmail.com>
2023-06-14 15:02:57 +0200

    argc_argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - function
+*@argc: integer
+*@argv: an array
+*Return: not always 0
+*/
+
+int main(int argc, char *argv[])
+{
+int a = 0;
+int b = 0;
+char d[] = "Error";
+
+if (argc != 2)
+{
+printf("%s\n", d);
+return (1);
+}
+a = atoi(argv[1]);
+while (a >= 25)
+{
+a -= 25;
+b++;
+}
+while (a >= 10)
+{
+a -= 10;
+b++;
+}
+while (a >= 5)
+{
+a -= 5;
+b++;
+}
+if (a == 1)
+b++;
+printf("%d\n", b);
+return (0);
+}
