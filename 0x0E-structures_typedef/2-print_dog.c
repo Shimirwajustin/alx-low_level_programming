f35bebaaf634589d12d2cf3e6fd3c329ec92dd5c
Shimirwa Justin <shimirwajustin@gmail.com>
2023-06-20 11:49:35 +0200

    typedef structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ return;
 if(d->name == NULL)
 d->name = "(nil)";
 if(d->owner == NULL)
-d->owner = "(nul)";
+d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
